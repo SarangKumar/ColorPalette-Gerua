ec7cce067a4011419f1131f69e6d170e1cd6ee04
SarangKumar <sarangkumar1578@gmail.com>
2023-01-19 17:22:08 +0530

    Made the header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <h3>
+            COLOR <b>UI</b>
+        </h3>
+        <ul>
+            <li>Home</li>
+            <li>More</li>
+            <li>Contact</li>
+        </ul>
+        <div>
+            {/* asdf */}
+        </div>
+    </div>
+  )
+}
+
+export default Header
